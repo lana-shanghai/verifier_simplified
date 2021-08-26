1492d7e20c69bda5a5c98785ba2de3f0a748a649
Lana <svetlana.konstantinovna@gmail.com>
2021-08-26 14:23:50 +0200

    increase weight

--- pallets/tradestorage/src/lib.rs
+++ pallets/tradestorage/src/lib.rs
@@ -89,13 +89,11 @@ pub mod tradestorage {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		#[pallet::weight(500_000 + T::DbWeight::get().reads_writes(1,1))]
+		#[pallet::weight(10_000_000 + T::DbWeight::get().reads_writes(1,1))]
 		pub fn store_trade_map(
 				origin:OriginFor<T>, 
 				//simulation_id:Vec<u8>, 
-				//market_id:Vec<u8>, 
-				simulation_id:u32, 
-				market_id:u32, 
+				//market_id:Vec<u8>,
 				market_slot: u32, 
 				//trade_id:Vec<u8>, 
 				//buyer: AccountId, 
