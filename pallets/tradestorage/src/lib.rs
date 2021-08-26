57584da4ed86516e2bf2ad1e4795676b3c44525c
Lana <svetlana.konstantinovna@gmail.com>
2021-08-26 13:17:28 +0200

    test if Vec<u8> causes the bad input error

--- pallets/tradestorage/src/lib.rs
+++ pallets/tradestorage/src/lib.rs
@@ -92,8 +92,10 @@ pub mod tradestorage {
 		#[pallet::weight(500_000 + T::DbWeight::get().reads_writes(1,1))]
 		pub fn store_trade_map(
 				origin:OriginFor<T>, 
-				simulation_id:Vec<u8>, 
-				market_id:Vec<u8>, 
+				//simulation_id:Vec<u8>, 
+				//market_id:Vec<u8>, 
+				simulation_id:u32, 
+				market_id:u32, 
 				market_slot: u32, 
 				//trade_id:Vec<u8>, 
 				//buyer: AccountId, 
